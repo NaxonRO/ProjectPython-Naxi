a32dc1ebb78bc8e813da2da042a6f136f230f9ec
NaxonRO <andrei@andrexnet.com>
2024-05-16 13:01:52 +0300

    rezolvare cerinta 4

---
+++ cerinta4.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+#Citim dim fisierul CSV
+df = pd.read_csv("train.csv")
+
+#Vedem care coloane au valori lipsa si determinam numarul acestora
+valoare_lipsa = df.isnull().sum()
+valoare_lipsa = valoare_lipsa[valoare_lipsa > 0]
+prop = (valoare_lipsa / len(df)) * 100
+
+#Printam coloanele cu valori lipsa
+print("\n\n\n")
+print("Coloanele cu valori lipsa:")
+print(valoare_lipsa)
+
+#Printam numarul si proportia valorilor lipsa
+print("\n\n\n")
+print("Numarul si proportia valorilor lipsa:")
+print(prop)
+
+#Dterminam procentul de valori lipsa
+surv = df[df['Survived'] == 1].isnull().sum() / len(df[df['Survived'] == 1]) * 100
+nesurv = df[df['Survived'] == 0].isnull().sum() / len(df[df['Survived'] == 0]) * 100
+
+#Printam procentul valorilor lipsa pentru clasa surv = 1
+print("\n\n\n")
+print("Procent valori lipsa surv == 1")
+print(surv)
+
+#Printam procentul valorilor lipsa pentru clasa surv = 0
+print("\n\n\n")
+print("Procent valori lipsa surv == 0")
+print(nesurv)
